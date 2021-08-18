91989a954cb362b6685067377f8c30efaf483d59
adipiciu <adipiciu2005@gmail.com>
2021-08-18 19:39:56 +0300

    Add files via upload

---
+++ TravianTaskQueue/TravianTaskQueue.meta.js
@@ -0,0 +1,33 @@
+﻿// ==UserScript==
+// @name        Travian Task Queue for Travian 4
+// @namespace   http://greasyfork.org/users/6214-adipiciu
+// @author      adipiciu (based on TTQ by Risi and further edited by Nevam and then Pimp Trizkit and Serj_LV)
+// @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @description Schedule delayed constructions, upgrades and attacks.
+// @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=56E2JM7DNDHGQ&item_name=TTQ4+script&currency_code=EUR
+// @include     *://*.travian.*
+// @include     *://*/*.travian.*
+// @exclude     *://*.travian*.*/hilfe.php*
+// @exclude     *://*.travian*.*/index.php*
+// @exclude     *://*.travian*.*/anleitung.php*
+// @exclude     *://*.travian*.*/impressum.php*
+// @exclude     *://*.travian*.*/anmelden.php*
+// @exclude     *://*.travian*.*/gutscheine.php*
+// @exclude     *://*.travian*.*/spielregeln.php*
+// @exclude     *://*.travian*.*/links.php*
+// @exclude     *://*.travian*.*/geschichte.php*
+// @exclude     *://*.travian*.*/tutorial.php*
+// @exclude     *://*.travian*.*/manual.php*
+// @exclude     *://*.travian*.*/ajax.php*
+// @exclude     *://*.travian*.*/ad/*
+// @exclude     *://*.travian*.*/chat/*
+// @exclude     *://wbb.forum.travian*.*
+// @exclude     *://*.travian*.*/activate.php*
+// @exclude     *://*.travian*.*/support.php*
+// @exclude     *://help.travian*.*
+// @exclude     *://*.answers.travian*.*
+// @exclude     *.css
+// @exclude     *.js
+
+// @version     1.8.6
+// ==/UserScript==
