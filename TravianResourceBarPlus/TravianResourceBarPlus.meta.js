d2c157bb79a7048c8bc8d2474ac5dbb13e73b2d8
adipiciu <adipiciu2005@gmail.com>
2021-08-18 19:37:53 +0300

    Add files via upload

---
+++ TravianResourceBarPlus/TravianResourceBarPlus.meta.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name           Travian Resource bar plus
+// @namespace      http://userscripts.org/scripts/show/75431
+// @description    Shows travian resources (for Travian Legends and Travian Shadow Empires)
+// @author         adipiciu (based on Travian Resource bar plus version 2.8.14 by Serj_LV)
+// @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=56E2JM7DNDHGQ&item_name=T4.4+script&currency_code=EUR
+// @include        *://*.travian.*
+// @include        *://*/*.travian.*
+// @exclude     *://*.travian*.*/hilfe.php*
+// @exclude     *://*.travian*.*/log*.php*
+// @exclude     *://*.travian*.*/index.php*
+// @exclude     *://*.travian*.*/anleitung.php*
+// @exclude     *://*.travian*.*/impressum.php*
+// @exclude     *://*.travian*.*/anmelden.php*
+// @exclude     *://*.travian*.*/gutscheine.php*
+// @exclude     *://*.travian*.*/spielregeln.php*
+// @exclude     *://*.travian*.*/links.php*
+// @exclude     *://*.travian*.*/geschichte.php*
+// @exclude     *://*.travian*.*/tutorial.php*
+// @exclude     *://*.travian*.*/manual.php*
+// @exclude     *://*.travian*.*/ajax.php*
+// @exclude     *://*.travian*.*/ad/*
+// @exclude     *://*.travian*.*/chat/*
+// @exclude     *://forum.travian*.*
+// @exclude     *://board.travian*.*
+// @exclude     *://shop.travian*.*
+// @exclude     *://*.travian*.*/activate.php*
+// @exclude     *://*.travian*.*/support.php*
+// @exclude     *://help.travian*.*
+// @exclude     *://*.answers.travian*.*
+// @exclude     *.css
+// @exclude     *.js
+
+// @version        2.21.17
+// ==/UserScript==
