76cc0eab3d2493eb052d8736d94af7a00c1f460e
adipiciu <adipiciu2005@gmail.com>
2021-08-18 19:39:05 +0300

    Add files via upload

---
+++ TravianWaveBuilder/TravianWaveBuilder.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name           Travian wave builder
+// @namespace      http://greasyfork.org/users/6214-adipiciu
+// @description    Wave builder for Travian Legends and Travian Shadow Empires
+// @author         adipiciu (based on Travian wave builder 0.5 by Serj_LV)
+// @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=56E2JM7DNDHGQ&item_name=Travian+wave+builder+script&currency_code=EUR
+// @include        *://*.travian.*/build.php*
+// @include        *://*.travian.*.*/build.php*
+// @include        *://*/*.travian.*/build.php*
+// @include        *://*/*.travian.*.*/build.php*
+
+// @version        1.9
+// ==/UserScript==
