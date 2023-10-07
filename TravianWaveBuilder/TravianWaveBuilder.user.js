5037dde629ee302ff40b1800ef4f82b0294f1608
adipiciu <adipiciu2005@gmail.com>
2023-10-07 20:31:07 +0300

    test

--- TravianWaveBuilder/TravianWaveBuilder.user.js
+++ TravianWaveBuilder/TravianWaveBuilder.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name           Travian wave builder
 // @namespace      https://github.com/adipiciu/Travian-scripts
-// @description    Wave builder for Travian Legends and Travian Glory of Sparta
+// @description    Wave builder for Travian Legends and Travian Shores of War
 // @author         adipiciu (based on Travian wave builder 0.5 by Serj_LV)
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=56E2JM7DNDHGQ&item_name=Travian+wave+builder+script&currency_code=EUR
 // @match          https://*.travian.com/build.php*
 
-// @version        2.7
+// @version        2.8
 // ==/UserScript==
 
 function allInOneOpera () {
 
-var version = '2.7';
+var version = '2.8';
 var scriptURL = 'https://github.com/adipiciu/Travian-scripts';
 var defInterval = 200;
 var sLang = detectLanguage();
