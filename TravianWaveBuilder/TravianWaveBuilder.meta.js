e0ce40b8d4a79d66677841f74200214007bdf50d
adipiciu <13155063+adipiciu@users.noreply.github.com>
2023-10-11 21:34:43 +0300

    fixed description

--- TravianWaveBuilder/TravianWaveBuilder.meta.js
+++ TravianWaveBuilder/TravianWaveBuilder.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Travian wave builder
 // @namespace      https://github.com/adipiciu/Travian-scripts
-// @description    Wave builder for Travian Legends and Travian Glory of Sparta
+// @description    Wave builder for Travian Legends and Travian Shores of War
 // @author         adipiciu (based on Travian wave builder 0.5 by Serj_LV)
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=56E2JM7DNDHGQ&item_name=Travian+wave+builder+script&currency_code=EUR
