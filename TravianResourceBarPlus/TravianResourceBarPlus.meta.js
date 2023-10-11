269713b0a2f8da4df434d8d1304cba467cf805c8
adipiciu <13155063+adipiciu@users.noreply.github.com>
2023-10-11 21:32:10 +0300

    fixed description

--- TravianResourceBarPlus/TravianResourceBarPlus.meta.js
+++ TravianResourceBarPlus/TravianResourceBarPlus.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Travian Resource bar plus
 // @namespace      https://github.com/adipiciu/Travian-scripts
-// @description    Shows travian resources (for Travian Legends and Travian Shadow Empires)
+// @description    Shows travian resources (for Travian Legends and Travian Shores of War)
 // @author         adipiciu (based on Travian Resource bar plus version 2.8.14 by Serj_LV)
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=56E2JM7DNDHGQ&item_name=T4.4+script&currency_code=EUR
