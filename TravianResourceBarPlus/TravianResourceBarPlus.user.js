7c2c381768ccfb3131f694c0de23a31a94d03115
adipiciu <13155063+adipiciu@users.noreply.github.com>
2026-05-13 19:07:21 +0300

    Fixed show needed resources for the new Travian update, Fixed link hints for Plus account, Minor fixes

--- TravianResourceBarPlus/TravianResourceBarPlus.user.js
+++ TravianResourceBarPlus/TravianResourceBarPlus.user.js
@@ -12,14 +12,14 @@
 // @exclude     *.css
 // @exclude     *.js
 
-// @version        2.26.10
+// @version        2.26.11
 // ==/UserScript==
 
 (function () {
 var RunTime = [Date.now()];
 
 function allInOneOpera () {
-var version = '2.26.10';
+var version = '2.26.11';
 
 notRunYet = false;
 
@@ -1067,7 +1067,7 @@ DICT = {
 		color4 : "Completamente Evoluido"
 	},
 	bg: { // Bulgarian language, thx Dushevadeca
-		// ingame messages 
+		// ingame messages
 		ok : "Потвърди",
 		cancel : "Отмени",
 		close : "Затвори",
@@ -2227,7 +2227,7 @@ DICT = {
 		sendres : "εμφάνισε «στείλε ύλες/στρατεύματα» εικονίδια",
 		sendmess : "εμφάνισε «στείλε μήνυμα» εικονίδιο",
 		analyzer : "World analyzer",
-		bigicon : "εμφάνισε πλατεία συγκέντρωσης ειδών",	
+		bigicon : "εμφάνισε πλατεία συγκέντρωσης ειδών",
 		addvtable : "Εμφάνιση επιπλέον πίνακα χωριών",
 		addvtableo : ['ανενεργό','ενεργό','κολλημένο'],
 		opennote : "άνοιξε αυτόματα σημειωματάριο",
@@ -2721,7 +2721,7 @@ function getUserID() {
 			var ad = ajaxNDIV(ajaxResp);
 			var aV = $xf('//td[contains(@class,"pla")]/a[contains(@href,"profile") and text() = "' + uName + '"]', 'f', ad);
 			ad = null;
-			if (aV) { 
+			if (aV) {
 				var uId = aV.href.match(/profile\/(\d+)/)[1];
 				uidcookie += uName +"\/@_"+ uId +"@@_";
 				RB_setValue(crtName + '-TRBP-UID', uidcookie);
@@ -3004,6 +3004,8 @@ darkCSS = "#content {background-color: "+dmColors[0]+" !important;}" +
 "#allianceBonusOverview div.bonusBox { background-color: "+dmColors[0]+";}" +
 "table.top5 thead td { background-color: "+dmColors[1]+";}" +
 "table.top5 tbody td { background-color: "+dmColors[0]+";}" +
+"table#regionalTop5 thead th { background-color: "+dmColors[1]+";}" +
+"table#regionalTop5 tbody td { background-color: "+dmColors[0]+";}" +
 "#allianceBonusOverview .progressBar .levels>div { opacity: 0.9;}" +
 //alliance //overview
 "table.events td { background-color: "+dmColors[0]+";}" +
@@ -3034,6 +3036,12 @@ darkCSS = "#content {background-color: "+dmColors[0]+" !important;}" +
 "div.village3 table tbody.troops tr td { background-color: "+dmColors[0]+";}" +
 //regions
 "table#neighboringRegions tbody tr:not(.highlight) td { background-color: "+dmColors[0]+";}" +
+//region overview
+"table.regionControlDetails thead td { background-color: "+dmColors[1]+";}" +
+"table.regionControlDetails tbody td { background-color: "+dmColors[0]+";}" +
+"div#regionPieChartAgenda table { background-color: "+dmColors[0]+" !important;}" +
+"div#regionPieChartAgenda table td { background-color: "+dmColors[0]+";}" +
+"div#statisticsVictoryTop5 table td { background-color: "+dmColors[0]+";}" +
 //production page
 "div#productionOverview table td { background-color: "+dmColors[0]+";}"
 
@@ -3128,7 +3136,7 @@ var dragMaster = (function() {
 		}
 	}
 	function mouseMove(e){
-		var ev = touchFL?e.touches[0]:e; 
+		var ev = touchFL?e.touches[0]:e;
 		if (mouseDownAt) if (Math.abs(mouseDownAt.x-ev.pageX)<10 && Math.abs(mouseDownAt.y-ev.pageY)<10) return;
 		with(dragObject.style) {
 			position = 'absolute';
@@ -3251,7 +3259,7 @@ function needed_show( base ) {
 		return timerB[j].obj;
 	}
 
-	var neededRes = base.match(/>(\d+).+?>(\d+).+?>(\d+).+?>(\d+)/);
+	var neededRes = base.querySelectorAll('span.value');
 	wfl = false;
 	var wantsResMem = [0,0,0,0,0,0,0,0,0,0];
 	var wantsResMemP = RB.wantsMem.slice();
@@ -3259,7 +3267,7 @@ function needed_show( base ) {
 	var forNPC = [0,0];
 	var beforeThis = $e('DIV');
 	for (var e = 0; e < 4; e++) {
-		wantsResMem[e+5] = parseInt(neededRes[e+1]);
+		wantsResMem[e+5] = parseInt(neededRes[e].textContent.replace(/([\u2000-\u20ff])/g,''));
 		wantsResMemP[e+5] = parseInt(wantsResMemP[e+5]) + wantsResMem[e+5];
 		var wantsRes = resNow[e] - wantsResMem[e+5];
 		var wantsResP = resNow[e] - wantsResMemP[e+5];
@@ -3297,7 +3305,7 @@ function needed_show( base ) {
 	return beforeThis;
 }
 
-function neededResAdd () {	
+function neededResAdd () {
 	function addNPC( base ) {
 		var gold = $gc('gold',base);
 		if( gold.length > 0 ) {
@@ -3308,8 +3316,8 @@ function neededResAdd () {
 	var baseWrap = $xf('.//div[contains(@class,"resourceWrapper")]','l',cont);
 	for( var i = 0; i < baseWrap.snapshotLength; i++ ) {
 		var base = baseWrap.snapshotItem(i);
-		if ( ! />(\d+).+?>(\d+).+?>(\d+).+?>(\d+)/.test(base.innerHTML) ) break;
-		var newD = needed_show( base.innerHTML );
+		if ( base.querySelector('span.value') == null ) break;
+		var newD = needed_show( base );
 		if (base.parentNode.classList.contains("contractWrapper") || base.parentNode.classList.contains("information") || base.parentNode.classList.contains("details") || (/hero/.test(crtPath))) {
 			addNPC(base.parentNode);
 			if ( !wfl && base.parentNode.getAttribute("class") == "information" ) continue;
@@ -3969,9 +3977,9 @@ function marketTradeRoutes() {
 		}
 		merUpd();
 	}
-	for (var i = 1; i < 5; i++ ) { 
+	for (var i = 1; i < 5; i++ ) {
 		var inp = $gn("r"+i,routesForm)[0];
-		inp.addEventListener('input', merUpd, false); 
+		inp.addEventListener('input', merUpd, false);
 		var divC = $e('DIV',[['style','margin:2px auto;font-size:24px;pointer-events:auto;']]);
 		var refM = $a(' - ',[["id","rbmin"],['href',jsVoid]]);
 		refM.addEventListener('click', mofLinkU, false);
@@ -4058,7 +4066,7 @@ function marketSummReal () {
 		var extRT = new Array();
 		for (i = 0; i < aT.length; i++) {
 			if (aT[i].classList.contains('history') || aT[i].classList.contains('return')) { aT[i].setAttribute('style','display:none;'); }
-			if (aT[i].classList.contains('history') || aT[i].classList.contains('return')) continue;	
+			if (aT[i].classList.contains('history') || aT[i].classList.contains('return')) continue;
 			if (aT[i].classList.contains('predicted')) {
 				// get time to go
 				var timerEl = $gc('timer',aT[i]);
@@ -4365,7 +4373,7 @@ function resSendOnMap () {
 		}
 		}
 	};
-	const observer = new MutationObserver(mutationCallback);		  
+	const observer = new MutationObserver(mutationCallback);
 	observer.observe(document.body, { childList: true, subtree: true });
 }
 
@@ -7475,7 +7483,7 @@ function buildDispatcher () {
 			  }
 			}
 		};
-		const observer = new MutationObserver(mutationCallback);		  
+		const observer = new MutationObserver(mutationCallback);
 		observer.observe(document.body, { childList: true, subtree: true });
 	} else if( gid == 'gid15' ) {
 		demolishSave();
@@ -8155,7 +8163,7 @@ function detectAttack () {
 			lastTimerB = timerNum+1;
 		}
 		if( RB.attackList.length < 2 ) {
-			aDv.innerHTML = 'No attack'; 
+			aDv.innerHTML = 'No attack';
 		} else {
 			aDv.innerHTML = '';
 		}
@@ -8241,9 +8249,9 @@ function detectAttack () {
 			var ad = ajaxNDIV(ajaxResp);
 			var move = $xf('.//div[contains(@class,"listEntry village") and contains(@class,"attack")]','f',ad);
 			ad = null;
-			if (move) { 
+			if (move) {
 				triggerAlarm();
-				return true; 
+				return true;
 			} else { noAttack(); }
 		}, showError);
 	}
@@ -8756,7 +8764,6 @@ function saveHeroPower () {
 			saveCookie( 'DictFL', 'dictFL' );
 		}
 	}
-	
 }
 
 function saveHeroMount () {
@@ -8772,7 +8779,7 @@ function saveHeroMount () {
 			});
 		});
 		observer.observe(hr, { childList: true, subtree: true });
-	
+
 		function checkHeroMount () {
 			if( hr.hasChildNodes() ) {
 				RB.dictFL[18] = 1;
@@ -8842,7 +8849,7 @@ function goldClubInfo () {
 		for( var t=0; t < ac.snapshotLength; t++ ) {
 			var inp = $gt('INPUT',ac.snapshotItem(t))[0];
 			if (inp.checked != chkbox.checked ) inp.click();
-		}	
+		}
 	}
 	function checkGreen () {
 		checkClass('attack_won_withoutLosses_small',this);
@@ -9182,14 +9189,14 @@ function messagesTopButtons () {
 			var markasreadButt = buttons[i];
 		}
 		if (buttons[i].name == 'delete') {
-			var delButt = buttons[i];	
+			var delButt = buttons[i];
 		}
 		if (buttons[i].name == 'archive') {
 			var archiveButton = buttons[i];
 		}
 	}
 	if (buttons.length > 0) {
-		if ( archiveButton ) { 
+		if ( archiveButton ) {
 			var newArcAbove = archiveButton.cloneNode(true);
 			ltr ? newArcAbove.style.marginLeft = "4px" : newArcAbove.style.marginRight = "4px";
 			messagesForm.prepend(newArcAbove);
@@ -9198,7 +9205,7 @@ function messagesTopButtons () {
 			var newDelAbove = delButt.cloneNode(true);
 			ltr ? newDelAbove.style.marginLeft = "4px" : newDelAbove.style.marginRight = "4px";
 			messagesForm.prepend(newDelAbove);
-		}	
+		}
 		if ( markasreadButt ) {
 			var newMarkAbove = markasreadButt.cloneNode(true);
 			messagesForm.prepend(newMarkAbove);
@@ -9458,8 +9465,8 @@ function createSemiLargerMapHTML() {
 }
 
 function displayWhatIsNew () {
-	if ($g('whatsnew')) { 
-		$g("whatsnew").style.visibility = "visible"; return; } 
+	if ($g('whatsnew')) {
+		$g("whatsnew").style.visibility = "visible"; return; }
 	else {
 		var box = $e('div',[['id','whatsnew'],['style','width:400px;position:fixed;top:50%;left:50%;transform: translate(-50%,-50%);color:black;background-color:'+rbpBckColor+';padding:5px 5px;border-radius:1em;z-index:999;opacity:0.95;']]);
 		var header = $e('div',[['style','height:35px;font-size:130%;font-weight:bold;text-align:center;']]);
@@ -9471,7 +9478,7 @@ function displayWhatIsNew () {
 		var donate = $ee('div',$a('Donate',[['href','https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=56E2JM7DNDHGQ&item_name=T4.4+script&currency_code=EUR'],['target','_blank']]),[['style','display:table-cell;width:33%;padding:5px;text-align:center;']]);
 		var closeb = $ee('div',$a('&#x2716;',[['style','font-size:140%;float:'+docDir[1]+';']]),[['style','height:15px;padding:10px;']]);
 		header.textContent = "About Travian Resource Bar+";
-		content.innerHTML = "<p><b>Changelog</b></p> <p>Version "+version+" - Apr 11, 2026:</p> <ul><li>Fixed oasis animals scan button in farm lists</li></ul> <p>Version 2.26.9 - Apr 3, 2026:</p> <ul><li>Fixes for the latest Travian update</li></ul> <p>Version 2.26.8 - Mar 12, 2026:</p> <ul><li>Updated the list of servers with rebalanced troops</li></ul> <p>Version 2.26.7 - Feb 21, 2026:</p> <ul><li>Fixed distance calculator for servers with rebalanced troops</li></ul>";
+		content.innerHTML = "<p><b>Changelog</b></p> <p>Version "+version+" - May 13, 2026:</p> <ul><li>Fixed show needed resources for the new Travian update</li><li>Fixed link hints for Plus account</li><li>Minor fixes</li></ul> <p>Version 2.26.10 - Apr 11, 2026:</p> <ul><li>Fixed oasis animals scan button in farm lists</li></ul> <p>Version 2.26.9 - Apr 3, 2026:</p> <ul><li>Fixes for the latest Travian update</li></ul> <p>Version 2.26.8 - Mar 12, 2026:</p> <ul><li>Updated the list of servers with rebalanced troops</li></ul> <p>Version 2.26.7 - Feb 21, 2026:</p> <ul><li>Fixed distance calculator for servers with rebalanced troops</li></ul>";
 		footer.appendChild(footerline);
 		footerline.appendChild(homepage);
 		footerline.appendChild(donate);
@@ -9533,7 +9540,6 @@ function displayWhatIsNew () {
 						if (T4_Variables.playableTribeIds[2]) RB.Setup[47] = 2; //Romans, Gauls, Teutons
 						if (T4_Variables.playableTribeIds[4]) RB.Setup[47] = 1; //Huns, Egyptians, Spartans, Vikings
 					}
-					
 				}
 				if (RB.Setup[48] == 0) { RB.Setup[48] = T4_Variables.Map.Size.width; }
 				if (RB.Setup[50] == 0) { RB.Setup[50] = T4_Variables.feature_flags.travelOverTheWorldEdge ? 1 : 2; }
@@ -9599,7 +9605,7 @@ function displayWhatIsNew () {
 	if( /dorf1.php/.test(crtPath) ) { troopsDorf1(); normalizeProduction(); }
 	villageHintEdit();
 	if( /dorf2.php/.test(crtPath) ) { parseDorf2(); if( RB.Setup[37] > 0 ) villageBMover(); }
-	if( /dorf3.php/.test(crtPath) ) villageHintDorf3();
+	if( /village\/statistics/.test(crtPath) ) { villageHintDorf3(); }
 	if( /(?:messages|report)/.test(crtPath) ) { viewMessageIW(); messagesTopButtons(); }
 	if( /report/.test(crtPath) ) reportsDelOrSearch(); else if( RB.overview[0] < -2 ) { RB.overview[0] = -1; saveCookie('OV', 'overview'); }
 	if( /messages\//.test(crtPath) ) { convertCoordsInMessagesToLinks(); }
@@ -9640,7 +9646,7 @@ function displayWhatIsNew () {
 		}
 		}
 	};
-	const observer = new MutationObserver(mutationCallback);		  
+	const observer = new MutationObserver(mutationCallback);
 	observer.observe($g('sidebarAfterContent'), { childList: true, subtree: true });
 	if( RB.Setup[14] > 0 ) showDorf1();
 	if( RB.Setup[12] > 0 ) showLinks();
